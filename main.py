dfb73fabc09a46dbcabf17d1413f15eb297e062e
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-04-19 21:44:10 +0300

    Add function for upload images to instagram

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from urllib.parse import urlsplit, unquote, urljoin
 import requests
 import urllib3
 from PIL import Image
+from dotenv import load_dotenv
+from instabot import Bot
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -54,27 +56,45 @@ def fetch_hubble_from_id(image_id, folder):
     download_image(image_url, image_title, folder)
 
 
-def modify_images(folder):
+def modify_images(downloaded_images_folder, modify_images_folder):
+    for filename in listdir(downloaded_images_folder):
+        image = Image.open(os.path.join(downloaded_images_folder, filename))
+        converted_image = image.convert('RGB')
+        width, height = converted_image.size
+        if width//height >= 1:
+            converted_image.thumbnail((1080, 1080))
+            converted_image.save(Path(modify_images_folder).joinpath(filename.with_suffix('.jpg')), format="JPEG")
+        else:
+            converted_image.thumbnail((width, 1080))
+            width, height = converted_image.size
+            coordinates = [-(1080-width)//2, 0, width+(1080-width)//2, height]
+            cropped_image = converted_image.crop(coordinates)
+            cropped_image.save(Path(modify_images_folder).joinpath(filename.with_suffix('.jpg')), format="JPEG")
+
+
+def upload_images_to_instagram(folder):
+    load_dotenv()
+    bot = Bot()
+    bot.login(username=os.environ['LOGIN'], password=os.environ['PASSWORD'])
     for filename in listdir(folder):
         filepath = os.path.join(folder, filename)
-        image = Image.open(filepath)
-        image.thumbnail((1080, 1080))
-        print(image.mode)
-        conver_image = image.convert('RGB')
-        conver_image.save(filepath, format="JPEG")
+        bot.upload_photo(filepath)
+    return 'Изображения -> опубликовано'
 
 
 def main():
-    images_folder = 'images'
+    downloaded_images_folder = 'images'
     Path(images_folder).mkdir(exist_ok=True)
+    modify_images_folder = 'modify_images' 
+    Path(modify_images_folder).mkdir(exist_ok=True)
 
     hubble_collection_name = 'holiday_cards'
 
     try:
-        fetch_spacex_last_launch(images_folder)
-        fetch_hubble_from_id('1', images_folder)
+        # fetch_spacex_last_launch(images_folder)
         fetch_hubble_from_collection(hubble_collection_name, images_folder)
-        modify_images(images_folder)
+        modify_images(downloaded_images_folder, modify_images_folder)
+        upload_image_to_instagram(modify_images_folder)
     except requests.exceptions.HTTPError as request_error:
         exit(f'Не могу получить ответ от сервера -> {request_error}')
 
